1bfc53b006131bdf3f49fee093d78b3763480552
wjlin0 <wjlgeren@163.com>
2022-07-02 13:04:56 +0800

    v4.0.1

--- run/getConfig.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# @Author   :wjlin0
-# @Blog     :https://wjlin0.com
-# @Email    :wjlgeren@163.com
-# @Time     :2022-06-29 10:45
-# @File     :getConfig
